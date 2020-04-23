58f36a0e9185b438b600ff29358011795c114365
bunbunjin <bunbunjin.aisu@gmail.com>
2020-04-23 21:20:24 +0900

    少し綺麗にした

--- pi.py
+++ pi.py
@@ -9,13 +9,7 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(25, GPIO.OUT)
 sound = ['voice1.mp3', 'voice2.mp3', 'voice3.mp3']
 itterassyai = ['voice4.mp3', 'voice5.mp3']
-morning = 'goodmorning.mp3'
-
-time_zone = datetime.now()
-str_date = str(time_zone)
-hour = str_date[8:-13]
-minute = str_date[14:-10]
-now = str_date[10:-11]
+morning = ['voice9.mp3', 'voice10.mp3', 'voice11.mp3', 'voice12.mp3']
 
 
 class Okaeri:
@@ -55,36 +49,51 @@ class Input_text:
 
     def __init__(self, input_text):
         self.input_text = input_text
+    
+    def go(self):
+        input_text = request.forms.input_text
+        print()
 
 
 @route('/pi', text='')
 def pi():
-    return template('alrame', text='')
+    return template('alram', text='')
 
 
 @route('/pi', method='POST')
 def pi():
-    text = '起動中'
-    input_text = request.forms.input_text
     while True:
+        time_zone = datetime.now()
+        str_date = str(time_zone)
+        hour = str_date[8:-13]
+        minute = str_date[14:-10]
+        now = str_date[11:-10]
         try:
             if GPIO.input(25) == GPIO.HIGH:
-                if now == input_text:
-                    while GPIO.input(25) == GPIO.HIGH:
-                        Alram(random).ran()
-                else:
-                    pass
-
+                input_text = request.forms.input_text
+                while GPIO.input(25) == GPIO.HIGH:
+                    if input_text == now:
+                        alram = Alram(morning)
+                        alram.ran()
+                        time_zone = datetime.now()
+                        str_date = str(time_zone)
+                        hour = str_date[8:-13]
+                        minute = str_date[14:-10]
+                        now = str_date[11:-10]
+                input_text = ''
+                
             else:
                 if hour == '07':
                     oha = Itterassyai(random)
                     oha.ran()
+                    
                 else:
                     ran_cho = Okaeri(random)
                     ran_cho.ran()
+
         except KeyboardInterrupt:
             pass
-        return template('alram', text=text, input_text=input_text)
+        return template('alram', text=input_text)
 
 
 run(host='localhost', port=8080, debug=True)
